e465481dc86b32fb7b0aeddf80a666f3f2797715
Faizan Ahmad <faizan855@gmail.com>
2025-02-21 14:50:01 +0500

    about page

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeroSection from "@/app/components/HeroSection";
+
+const About = () => {
+    return (
+        <>
+            <HeroSection title={"OUR STORY"} imageUrl={"/about1.svg"}/>
+        </>
+    );
+};
+
+export default About;
